1fa2fbbd7aa82c7d6096fea55b883723279a22ec
Queenshiba <haruka0111.2a@gmail.com>
2021-08-07 23:57:57 +0900

    add table

--- dist/main.js
+++ dist/main.js
@@ -6,11 +6,35 @@ fetch("./target.json")
     })
     .then(function (data) {
         const originaltarget = data.target1900
-        const [num, eng, jpn] = [originaltarget.number, originaltarget.eng, originaltarget.jpn];
         console.log(data.target1900)
-        console.log(typeof data.target1900)
-        let result = data.find(obj => {
-            return obj.eng
-        })
-        console.log(result)
+
+        originaltarget.forEach(index => {
+            let num = index.number
+            let eng = index.eng
+            let jpn = index.jpn;
+            // console.log(index)
+        });
+
+        // make a test btn
+        let makeATestBtn = document.getElementById('makeATestBtn')
+        makeATestBtn.addEventListener("click", () => makeATest())
+
+        function makeATest() {
+            originaltarget.forEach(index => {
+                let num = index.number
+                let eng = index.eng
+                let jpn = index.jpn;
+                // console.log(index)
+
+                let createTr = document.createElement('tr')
+                createTr.setAttribute("class", "row")
+                let createTd = document.createElement('td')
+                createTd = "hey"
+                let table = document.getElementById('table')
+                table.appendChild(createTr)
+                createTr.appendChild(createTd)
+            });
+
+
+        }
     })
