608cb5beca3893b463bcd55536fd216c33e3d0a3
Queenshiba <haruka0111.2a@gmail.com>
2021-08-11 23:05:30 +0900

    add systanPhrase json

---
+++ dist/systanPhrase.js
@@ -0,0 +1,10 @@
+"use strict";
+
+fetch("./systanPhrase.json")
+    .then(function (resp) {
+        return resp.json();
+    })
+    .then(function (data) {
+        const originalsystanPhrase = data.systanPhrase
+        console.log(originalsystanPhrase)
+    })
